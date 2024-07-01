26c4e8a51466e9c3ee0e07640cd64a77135c2922
lalifeier <lalifeier@gmail.com>
2024-07-01 14:43:06 +0000

    init

--- main.py
+++ main.py
@@ -24,8 +24,8 @@ async def hello_world():
 async def get_play_url(channel_id: str, video_id: str):
     logger.info(f"Received request for channel ID: {channel_id} and video ID: {video_id}")
 
-    # is_m3u8 = video_id.endswith(".m3u8")
-    # video_id = video_id[:-5] if is_m3u8 else video_id
+    is_m3u8 = video_id.endswith(".m3u8")
+    video_id = video_id[:-5] if is_m3u8 else video_id
 
     try:
         channel = CHANNEL_MAPPINGS.get(channel_id, channel_id)
@@ -37,54 +37,54 @@ async def get_play_url(channel_id: str, video_id: str):
             logger.warning(f"Play URL not found for video ID: {video_id}")
             raise HTTPException(status_code=404, detail="Play URL not found")
 
-        # if is_m3u8:
-        if isinstance(play_url, str):
-            if play_url.startswith("#EXTM3U"):
+        if is_m3u8:
+            if isinstance(play_url, str):
+                if play_url.startswith("#EXTM3U"):
+                    headers = {
+                        "Content-Type": "application/vnd.apple.mpegurl",
+                        "Content-Disposition": f'attachment; filename="{int(time.time())}.m3u8"'
+                    }
+                    return StreamingResponse(iter([play_url.encode()]), headers=headers)
+                else:
+                    logger.info(f"Redirecting to Play URL: {play_url}")
+                    return RedirectResponse(url=play_url, status_code=302)
+            elif isinstance(play_url, bytes):
                 headers = {
-                    "Content-Type": "application/vnd.apple.mpegurl",
-                    "Content-Disposition": f'attachment; filename="{int(time.time())}.m3u8"'
+                    "Content-Type": "video/MP2T",
+                    "Content-Disposition": f'attachment; filename="{int(time.time())}.ts"'
                 }
-                return StreamingResponse(iter([play_url.encode()]), headers=headers)
+                logger.info(f"Returning TS content for video ID: {video_id}")
+                return StreamingResponse(iter([play_url]), headers=headers)
             else:
-                logger.info(f"Redirecting to Play URL: {play_url}")
-                return RedirectResponse(url=play_url, status_code=302)
-        elif isinstance(play_url, bytes):
-            headers = {
-                "Content-Type": "video/MP2T",
-                "Content-Disposition": f'attachment; filename="{int(time.time())}.ts"'
-            }
-            logger.info(f"Returning TS content for video ID: {video_id}")
-            return StreamingResponse(iter([play_url]), headers=headers)
-        else:
-            logger.error(f"Unexpected response type for video ID: {video_id}")
-            raise HTTPException(status_code=500, detail="Unexpected response type")
+                logger.error(f"Unexpected response type for video ID: {video_id}")
+                raise HTTPException(status_code=500, detail="Unexpected response type")
 
-        # else:
-        #     if isinstance(play_url, str):
-        #         if play_url.startswith("#EXTM3U"):
-        #             play_url = '/{channel_id}/{video_id}.m3u8'
+        else:
+            if isinstance(play_url, str):
+                if play_url.startswith("#EXTM3U"):
+                    play_url = '/{channel_id}/{video_id}.m3u8'
                     
-        #         player_html = f"""
-        #         <!DOCTYPE html>
-        #         <html>
-        #         <head>
-        #             <meta charset="UTF-8">
-        #             <title>Video Player</title>
-        #             <link rel="stylesheet" href="https://unpkg.com/artplayer/dist/artplayer.css">
-        #             <script src="https://unpkg.com/artplayer/dist/artplayer.js"></script>
-        #         </head>
-        #         <body>
-        #             <div id="player"></div>
-        #             <script>
-        #                 var art = new Artplayer({{
-        #                     container: '#player',
-        #                     url: '{play_url}',
-        #                 }});
-        #             </script>
-        #         </body>
-        #         </html>
-        #         """
-        #         return HTMLResponse(content=player_html)
+                player_html = f"""
+                <!DOCTYPE html>
+                <html>
+                <head>
+                    <meta charset="UTF-8">
+                    <title>Video Player</title>
+                    <link rel="stylesheet" href="https://unpkg.com/artplayer/dist/artplayer.css">
+                    <script src="https://unpkg.com/artplayer/dist/artplayer.js"></script>
+                </head>
+                <body>
+                    <div id="player"></div>
+                    <script>
+                        var art = new Artplayer({{
+                            container: '#player',
+                            url: '{play_url}',
+                        }});
+                    </script>
+                </body>
+                </html>
+                """
+                return HTMLResponse(content=player_html)
 
     except ModuleNotFoundError as e:
         logger.error(f"Module {channel} not found: {e}")
