de45b33c413f9380f9f0ed0bfc5cff1db060cac5
lalifeier <lalifeier@gmail.com>
2024-07-06 17:02:17 +0000

    init

--- live/api/bilibili.py
+++ live/api/bilibili.py
@@ -6,8 +6,19 @@ from live.util.http import get_json
 logger = logging.getLogger(__name__)
 
 class BiliBili(BaseChannel):
-
     async def get_play_url(self, video_id: str) -> Optional[str]:
+        json_data = await get_json( f"https://api.live.bilibili.com/room/v1/Room/room_init?id={video_id}")
+
+        if json_data["msg"] == "直播间不存在":
+            return None
+        
+        data = json_data.get("data", {})
+        live_status = data.get("live_status")
+        if live_status != 1:
+            return None
+        
+        video_id = str(int(data["room_id"]))
+        
         params = {
             "room_id": video_id,
             "protocol": "0,1",
