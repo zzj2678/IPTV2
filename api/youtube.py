a33984c6c4a97c9d63f1a6ea8356134584aa45ed
lalifeier <lalifeier@gmail.com>
2024-07-06 11:33:01 +0000

    init

--- api/youtube.py
+++ api/youtube.py
@@ -3,7 +3,9 @@ from typing import Optional
 from .base import BaseChannel
 import json
 from urllib.parse import urljoin
+import re
 from utils.http import get_json, post_json, get_text
+from utils.m3u8 import update_m3u8_content
 
 logger = logging.getLogger(__name__)
 
@@ -23,19 +25,53 @@ class Youtube(BaseChannel):
 
         return await post_json(url, json=data)
 
+    async def get_max_resolution_url(self, url):
+        text = await get_text(url)
+
+        lines = text.splitlines()
+
+        max_resolution = 0
+        max_resolution_url = None
+
+        for i, line in enumerate(lines):
+            if line.startswith("#") and 'RESOLUTION=' in line:
+                resolution_match = re.search(r'RESOLUTION=(\d+)x(\d+)', line)
+                if resolution_match:
+                    width = int(resolution_match.group(1))
+                    height = int(resolution_match.group(2))
+                    resolution = width * height
+
+                    if resolution > max_resolution:
+                        max_resolution = resolution
+                        max_resolution_url = lines[i + 1] 
+
+        return max_resolution_url
+
     async def get_play_url(self, video_id: str) -> Optional[str]:
         data = await self.get_player(video_id)
 
         streaming_data = data.get('streamingData', {})
 
-        print(streaming_data)
+        # print(json.dumps(streaming_data))
 
         hls_manifest_url = streaming_data.get('hlsManifestUrl')
-        if hls_manifest_url:
-            resolution_url = await get_text(hls_manifest_url)
-            return resolution_url
+        if not hls_manifest_url:
+            return None
 
-        return None
+        url = await self.get_max_resolution_url(hls_manifest_url)
+
+        # headers = {"Referer": 'https://www.youtube.com/'}
+        # m3u8_content = await get_text(url)
+
+        # m3u8_content = update_m3u8_content(url, m3u8_content, True)
+
+        # print(m3u8_content)
+
+        return url
+
+     
+
+  
 
 site = Youtube()
 
